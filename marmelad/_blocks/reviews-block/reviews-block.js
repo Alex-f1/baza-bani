8a4521227931fc564256cf066e4f61422d52f84b
Alex <skyriver.f1@gmail.com>
2023-04-05 04:06:38 +0500

    added reviews block & more

---
+++ marmelad/_blocks/reviews-block/reviews-block.js
@@ -0,0 +1,46 @@
+const reviewBlockSlider = new Swiper(".js-reviews-block-slider", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  allowTouchMove: false,
+  loop: true,
+  pagination: {
+    el: ".main-swiper-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+});
+
+const buttonPrev = document.querySelector(".js-swiper-button-prev"); 
+const buttonNext = document.querySelector(".js-swiper-button-next"); 
+
+buttonPrev.addEventListener("click",function () {
+  reviewBlockSlider.slidePrev();
+});
+buttonNext.addEventListener("click",function () {
+  reviewBlockSlider.slideNext();
+});
+
+
+const reviewThumbNestedSlider = new Swiper(".js-review-thumb-nested-slider", {
+  slidesPerView: 4,
+  spaceBetween: 14,
+  pagination: {
+    el: ".review-thumb__pics .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".review-thumb__pics .swiper-button-next",
+    prevEl: ".review-thumb__pics .swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+  },
+});
