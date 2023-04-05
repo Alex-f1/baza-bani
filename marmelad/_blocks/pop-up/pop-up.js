21b0a8a97906e55da5e6be32e6df664a52048d63
Alex <skyriver.f1@gmail.com>
2023-04-05 19:29:28 +0500

    added video popup

--- marmelad/_blocks/pop-up/pop-up.js
+++ marmelad/_blocks/pop-up/pop-up.js
@@ -1,3 +1,17 @@
 const aboutService = new HystModal({
   linkAttributeName: "data-popup-modal",
+  afterClose: function (modal) {
+    document.querySelector(".js-video-popup iframe")
+      .setAttribute("src", "");
+  },
+});
+
+const buttonPlayVideo = document.querySelectorAll(".js-button-play");
+
+buttonPlayVideo.forEach(function(element) {
+  element.addEventListener("click", function (event) {
+    let getVideoSrc = event.currentTarget.getAttribute('data-video-src');
+    document.querySelector(".js-video-popup iframe")
+      .setAttribute("src", getVideoSrc);
+  })
 });
