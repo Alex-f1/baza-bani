c3eae21aa941b708fb7c6c6cc23452628afb26d0
Alex <skyriver.f1@gmail.com>
2023-04-07 13:06:16 +0500

    added card project block & page & more blocks

--- marmelad/_blocks/reviews-block/reviews-block.js
+++ marmelad/_blocks/reviews-block/reviews-block.js
@@ -1,50 +1,56 @@
-const reviewBlockSlider = new Swiper(".js-reviews-block-slider", {
-  slidesPerView: 1,
-  spaceBetween: 50,
-  allowTouchMove: false,
-  loop: true,
-  pagination: {
-    el: ".main-swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      allowTouchMove: true,
+function reviewsBlockSliderInit() {
+  const reviewBlockSlider = new Swiper(".js-reviews-block-slider", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    allowTouchMove: false,
+    loop: true,
+    pagination: {
+      el: ".main-swiper-pagination",
+      clickable: true,
     },
-    1280: {
-      allowTouchMove: false,
+    breakpoints: {
+      320: {
+        allowTouchMove: true,
+      },
+      1280: {
+        allowTouchMove: false,
+      },
     },
-  },
-});
-
-const buttonPrev = document.querySelector(".js-swiper-button-prev"); 
-const buttonNext = document.querySelector(".js-swiper-button-next"); 
-
-buttonPrev.addEventListener("click",function () {
-  reviewBlockSlider.slidePrev();
-});
-buttonNext.addEventListener("click",function () {
-  reviewBlockSlider.slideNext();
-});
-
-
-const reviewThumbNestedSlider = new Swiper(".js-review-thumb-nested-slider", {
-  slidesPerView: 4,
-  spaceBetween: 14,
-  pagination: {
-    el: ".review-thumb__pics .swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".review-thumb__pics .swiper-button-next",
-    prevEl: ".review-thumb__pics .swiper-button-prev",
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
+  });
+  
+  const buttonPrev = document.querySelector(".js-swiper-button-prev"); 
+  const buttonNext = document.querySelector(".js-swiper-button-next"); 
+  
+  buttonPrev.addEventListener("click",function () {
+    reviewBlockSlider.slidePrev();
+  });
+  buttonNext.addEventListener("click",function () {
+    reviewBlockSlider.slideNext();
+  });
+  
+  
+  const reviewThumbNestedSlider = new Swiper(".js-review-thumb-nested-slider", {
+    slidesPerView: 4,
+    spaceBetween: 14,
+    pagination: {
+      el: ".review-thumb__pics .swiper-pagination",
+      clickable: true,
     },
-    768: {
-      slidesPerView: 4,
+    navigation: {
+      nextEl: ".review-thumb__pics .swiper-button-next",
+      prevEl: ".review-thumb__pics .swiper-button-prev",
     },
-  },
-});
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
+if (document.querySelectorAll(".js-reviews-block-slider").length || document.querySelectorAll(".js-review-thumb-nested-slider").length) {
+  reviewsBlockSliderInit();
+}
