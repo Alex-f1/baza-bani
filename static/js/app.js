b93df4a99e26533a9896673b0c8e418e35ed2466
Alex <skyriver.f1@gmail.com>
2023-04-09 00:13:53 +0500

    added more blocks in card project block

--- static/js/app.js
+++ static/js/app.js
@@ -25,7 +25,33 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 /* ^^^
  * Scripts
  * ========================================================================== */
+// function layoutOptions() {
+//   const layoutOption = document.querySelectorAll(".js-card-project-layout-option")
+//   layoutOption.forEach(function(element, index) {
+//     element.addEventListener("click", function(event) {
+//       event.preventDefault();
+//       event.currentTarget.classList.add("_is-active");
+//       event.currentTarget.closest(".card-project-layout-options__nav").querySelectorAll(".js-card-project-layout-option").forEach( function(elementInner) {
+//         if (element.classList.contains("_is-active")) {
+//           elementInner.classList.remove("_is-active");
+//         }
+//       })
+//       console.log(event);
+//     })
+//   })
+// } 
+// layoutOptions();
 
+
+const layoutOptionsNavigationScrollbar = new Swiper(".js-layout-options-navigation-scrollbar", {
+  direction: "horizontal",
+  slidesPerView: "auto",
+  freeMode: true,
+  scrollbar: {
+    el: ".js-layout-options-navigation-scrollbar .swiper-scrollbar",
+    draggable: true
+  }
+});
 
 function inputFile() {
   var inputs = document.querySelectorAll('.input-file');
