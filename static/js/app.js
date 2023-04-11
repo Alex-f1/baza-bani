ef47ea11a63d259e7d736ae99605d66b335e8f69
Alex <skyriver.f1@gmail.com>
2023-04-12 03:52:46 +0500

    added gallery page & more

--- static/js/app.js
+++ static/js/app.js
@@ -267,6 +267,35 @@ if (document.querySelectorAll('.phone-mask').length) {
   phoneMask();
 }
 
+function galleryBlock() {
+  const buttonGalleryPhotos = document.querySelectorAll(".js-button-gallery-photos");
+  const galleryPhotos = document.querySelectorAll(".js-gallery-photos");
+
+  for (let i = 0; i < buttonGalleryPhotos.length; i++) {
+    buttonGalleryPhotos[i].addEventListener("click", event => {
+      event.preventDefault();
+      let buttonGalleryPhotosChildren = event.currentTarget.parentElement.children;
+
+      for (let b = 0; b < buttonGalleryPhotosChildren.length; b++) {
+        buttonGalleryPhotosChildren[b].classList.remove("_is-active", "button-default--outline");
+      }
+
+      buttonGalleryPhotos[i].classList.add("_is-active", "button-default--outline");
+      let layoutChildren = event.target.closest(".gallery-block__navigation").nextElementSibling.children;
+
+      for (let l = 0; l < layoutChildren.length; l++) {
+        layoutChildren[l].classList.remove("_is-show");
+      }
+
+      galleryPhotos[i].classList.add("_is-show");
+    });
+  }
+}
+
+if (document.querySelectorAll(".gallery-block--self").length) {
+  galleryBlock();
+}
+
 function heroSliderInit() {
   const item = document.querySelectorAll(".hero__item");
   const numberZero = item.length + 1 <= 10 ? "0" : "";
