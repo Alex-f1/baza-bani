3cb5faed23b95bbd9666f5abdbdac15b2c7d776b
Alex <skyriver.f1@gmail.com>
2023-04-11 20:04:29 +0500

    added faq block & faq form & more

--- static/js/app.js
+++ static/js/app.js
@@ -159,6 +159,43 @@ if (document.querySelectorAll(".card-project-additional-options").length) {
   additionalOptions();
 }
 
+function faqBlock() {
+  const answer = document.querySelectorAll(".faq-block__answer");
+  answer.forEach(function (element) {
+    element.querySelectorAll("p").forEach(function (childrenElement) {
+      childrenElement.style.display = "block";
+    });
+    let getHeightElement = element.querySelector("p").clientHeight;
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      if (getHeightElement > 75) {
+        element.style.height = 65 + "px";
+        element.classList.add("_is-collapsed");
+        element.addEventListener("click", function () {
+          element.classList.remove("_is-collapsed");
+          element.style.height = "auto";
+          element.closest(".faq-block__item").style.height = "auto";
+        });
+      }
+
+      return;
+    }
+
+    if (getHeightElement > 75) {
+      element.style.height = 98 + "px";
+      element.classList.add("_is-collapsed");
+      element.addEventListener("click", function () {
+        element.classList.remove("_is-collapsed");
+        element.style.height = "auto";
+      });
+    }
+  });
+}
+
+if (document.querySelectorAll(".faq-block").length) {
+  faqBlock();
+}
+
 function filterNavigation() {
   const filterNavigationScrollbar = new Swiper(".js-filter-navigation-scrollbar", {
     direction: "horizontal",
